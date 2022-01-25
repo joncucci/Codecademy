5a2847232ecad7588ec5e4b6b738d96d26390268
Jon Cucci <joncucci@gmail.com>
2022-01-24 22:10:28 -0500

    ufo

---
+++ Intermediate C++/ufo/ufo_functions.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+void display_misses(int misses);
+void greet();
+bool end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
